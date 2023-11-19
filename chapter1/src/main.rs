7c634c8c89ab2f3e076fc4825959194edf596db4
Kevin Oberlies <favilo@gmail.com>
2023-11-19 12:35:53 -0800

    Archive extraction and fetching the extracted directory from the tar archive

--- chapter1/src/main.rs
+++ chapter1/src/main.rs
@@ -1,14 +1,17 @@
 use std::path::PathBuf;
 
 use color_eyre::eyre::{Context, Result};
+use tardyai::{untar_images, Url};
 
 fn main() -> Result<()> {
     env_logger::Builder::new()
         .filter_level(log::LevelFilter::Info)
         .init();
     color_eyre::install()?;
-    let path: PathBuf = tardyai::untar_images(tardyai::Url::Pets)
+
+    let path: PathBuf = untar_images(Url::Pets)
         .context("downloading Pets")?
         .join("images");
+    log::info!("Images are in: {}", path.display());
     Ok(())
 }
